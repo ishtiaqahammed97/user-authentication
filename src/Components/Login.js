584c5069435f0024e468f90cb49d0be29b9d2dd7
ishtiaq <isah2080@ymail.com>
2022-02-21 17:43:31 +0600

    new feature facebook authentication added

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import {
     getAuth, signOut,
     createUserWithEmailAndPassword, signInWithEmailAndPassword,
-    signInWithPopup, GoogleAuthProvider,
+    signInWithPopup, GoogleAuthProvider, FacebookAuthProvider
 } from "firebase/auth";
 import { initializeApp } from 'firebase/app';
 import firebaseConfig from './firebase.config';
@@ -89,24 +89,24 @@ const Login = () => {
     };
 
     // sign in with google
-    // const handleGoogleSignIn = () => {
-    //     const provider = new GoogleAuthProvider();
-    //     // remember to give the first parameter auth, then the
-    //     // second parameter would be provider, it gave you a tough time
-    //     signInWithPopup(auth, provider)
-    //         .then((res) => {
-    // const { displayName, photoURL, email } = res.user;
-    // const signedInUser = {
-    //     name: displayName,
-    //     photo: photoURL,
-    //     email: email,
-    //     isSignedIn: true
-    // };
-    // setUser(signedInUser);
-    //         }).catch((err) => {
-    //             console.log(err);
-    //         })
-    // };
+    const handleGoogleSignIn = () => {
+        const provider = new GoogleAuthProvider();
+        // remember to give the first parameter auth, then the
+        // second parameter would be provider, it gave you a tough time
+        signInWithPopup(auth, provider)
+            .then((res) => {
+                const { displayName, photoURL, email } = res.user;
+                const signedInUser = {
+                    name: displayName,
+                    photo: photoURL,
+                    email: email,
+                    isSignedIn: true
+                };
+                setUser(signedInUser);
+            }).catch((err) => {
+                console.log(err);
+            })
+    };
 
     // sign in user
     const handleSignIn = (e) => {
@@ -123,8 +123,6 @@ const Login = () => {
                     isSignedIn: true
                 };
                 setUser(signedInUser);
-                console.log(user)
-                // ...
             })
             .catch((error) => {
                 const errorCode = error.code;
@@ -134,21 +132,43 @@ const Login = () => {
         e.preventDefault();
     };
 
+    /// handle facebook sign in with popup
+
+    const fbProvider = new FacebookAuthProvider()
+
+    const handleFacebookSignIn = () => {
+        signInWithPopup(auth, fbProvider)
+            .then((result) => {
+                const user = result.user;
+                const { displayName, photoURL, email } = user;
+                const signedInUser = {
+                    name: displayName,
+                    email: email,
+                    photo: photoURL,
+                    isSignedIn: true
+                };
+                setUser(signedInUser);
+                // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+                const credential = FacebookAuthProvider.credentialFromResult(result);
+                const accessToken = credential.accessToken;
+
+            })
+            .catch((error) => {
+                // Handle Errors here.
+                const errorMessage = error.message;
+                console.log(errorMessage);
+            });
+    }
+
     return (
         <div className="container d-flex">
-            {/* <div>
-                <h3>This is google auth</h3>
-                {user.isSignedIn ? <button onClick={() => handleSignOut()}>Sign out</button>
-                    : <button onClick={() => handleGoogleSignIn()}>Google sign in</button>}
 
-                {user.isSignedIn && <div>
+            <div>
+                {user.isSignedIn ? <div className="welcome-msg">
                     <p>Welcome {user.name}</p>
                     <p>Email: {user.email}</p>
                     <img src={user.photo} alt="" ></img>
-                </div>}
-            </div> */}
-            <div>
-                {user.isSignedIn ? <h3 className="welcome-msg">Welcome : {user.email}</h3> : <h3 className="welcome-msg">Please Login First</h3>}
+                </div> : <h3 className="welcome-msg">Please Login First</h3>}
             </div>
             <div className="own-auth">
                 {!newUser ? <h1 className="text-center text-secondary">Login Form</h1> : <h1 className="text-center text-secondary">Sign In Form</h1>}
@@ -166,7 +186,21 @@ const Login = () => {
                 </form>
                 <button className="btn btn-success btn-block mb-2 mt-5 ms-5"
                     onClick={registerUser}
-                >Don't have account? Register here</button>
+                >Don't have account? Register here
+                </button> &nbsp;  <div>
+
+                    <button onClick={() => handleGoogleSignIn()}
+                        className="btn btn-secondary ms-5 mb-2"
+                    >
+                        Google Sign in
+                    </button>
+
+                    <button
+                        className="btn btn-secondary mb-2 ms-2"
+                        onClick={handleFacebookSignIn}
+                    >Facebook Sign in</button>
+
+                </div>
             </div>
         </div>
     );
